5b01744683edfc94df916afe9de981382f44c77b
yosan <yosankharel32@gmail.com>
2023-03-27 20:13:35 +0545

    chat fixing

--- lib/screens/appointment.dart
+++ lib/screens/appointment.dart
@@ -214,6 +214,7 @@ class _AppointmentState extends State<Appointment> {
                                                     }
                                                   },
                                                   icon: Icon(Icons.chat,
+
                                                       color: Colors.purple),
                                                   label: Text("Chat"),
                                                 )
